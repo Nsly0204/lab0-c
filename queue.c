f5ff4be2d31003f3fef20f6d2d259c971776c474
Derron Lee <tjlee1589@gmail.com>
2025-03-02 16:32:55 +0800

    Recover code after chaos of merging
    During last commit, my code is totally out of my control,
    this commit is to commit the correct code copy in my
    computer.
    
    Change-Id: Ie7535d3d5b589a17ebdc799c5aaf0fbbb5e84241

--- queue.c
+++ queue.c
@@ -4,49 +4,163 @@
 
 #include "queue.h"
 
+/* Notice: sometimes, Cppcheck would find the potential NULL pointer bugs,
+ * but some of them cannot occur. You can suppress them by adding the
+ * following line.
+ *   cppcheck-suppress nullPointer
+ */
+
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *head = malloc(sizeof(struct list_head));
+    if (!head) {
+        return NULL;
+        // https://hackmd.io/@sysprog/linux-macro-containerof
+    }
+    INIT_LIST_HEAD(head);
+    return head;
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    if (!head)
+        return;
+
+    element_t *entry = NULL, *safe = NULL;
+    list_for_each_entry_safe (entry, safe, head, list) {
+        q_release_element(entry);
+    }
+    free(head);
+    return;
+    // https://hackmd.io/@sysprog/linux-macro-containerof#Linux-核心原始程式碼巨集-container_of
+    // www.catb.org/esr/structure-packing/
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    if (!head || !s)
+        return false;
+
+    element_t *ele = malloc(sizeof(element_t));
+    if (!ele)
+        return false;
+
+    char *val = malloc((strlen(s) + 1) * sizeof(char));
+    if (!val) {
+        free(ele);
+        return false;
+    }
+    // https://hackmd.io/@sysprog/HJmB5x5Hn
+
+    memcpy(val, s, strlen(s) + 1);
+    ele->value = val;
+
+    list_add(&ele->list, head);  // notice the priority of operators
+    // https://stackoverflow.com/questions/62943547/how-does-the-list-add-function-for-linux-kernel-linked-lists-work
+
     return true;
 }
 
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    /*
+    static inline void list_add_tail(struct list_head *node, struct list_head
+    *head) { struct list_head *prev = head->prev;
+
+    prev->next = node;
+    node->next = head;
+    node->prev = prev;
+    head->prev = node;
+    }
+    */
+    if (!head || !s)
+        return false;
+
+    element_t *ele = malloc(sizeof(element_t));
+    if (!ele)
+        return false;
+
+    char *val = malloc((strlen(s) + 1) * sizeof(char));
+    if (!val) {
+        free(ele);
+        return false;
+    }
+
+    memcpy(val, s, strlen(s) + 1);
+    ele->value = val;
+    list_add_tail(&ele->list, head);
+
     return true;
 }
 
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+    element_t *tmp = list_first_entry(head, element_t, list);
+    // compare list_del_init and list_del
+    list_del_init(&tmp->list);
+    // comparison of strlcpy/snprintf/memcpy
+    // https://blog.csdn.net/xx326664162/article/details/72781528
+    // https://hackmd.io/@sysprog/HJmB5x5Hn
+    if (sp)
+        snprintf(sp, bufsize, "%s", tmp->value);
+    return tmp;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+    element_t *tmp = list_last_entry(head, element_t, list);
+    list_del_init(&tmp->list);
+    if (sp)
+        snprintf(sp, bufsize, "%s", tmp->value);
+    return tmp;
 }
 
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    if (!head)
+        return 0;
+    int len = 0;
+    struct list_head *li;
+
+    /*
+    #define list_for_each(node, head) \
+    for (node = (head)->next; node != (head); node = node->next)
+    */
+    list_for_each (li, head)
+        len++;
+
+    return len;
 }
 
 /* Delete the middle node in queue */
 bool q_delete_mid(struct list_head *head)
 {
     // https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+    if (!head || list_empty(head))
+        return false;
+    struct list_head *right, *left;
+    right = head->next;
+    left = head->prev;
+
+    while (right != left && left->prev != right) {
+        right = right->next;
+        left = left->prev;
+    }
+
+    list_del(left);
+    q_release_element(list_entry(left, element_t, list));
+
     return true;
 }
 
@@ -54,8 +168,6 @@ bool q_delete_mid(struct list_head *head)
 bool q_delete_dup(struct list_head *head)
 {
     // https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
-<<<<<<< HEAD
-=======
     if (!head || list_empty(head))
         return false;
     if (list_is_singular(head))
@@ -80,7 +192,6 @@ bool q_delete_dup(struct list_head *head)
     // why recursion
     // https://hackmd.io/@sysprog/c-recursion
 
->>>>>>> e417627 (Help commit)
     return true;
 }
 
@@ -88,17 +199,42 @@ bool q_delete_dup(struct list_head *head)
 void q_swap(struct list_head *head)
 {
     // https://leetcode.com/problems/swap-nodes-in-pairs/
+    if (!head || list_empty(head))
+        return;
+    struct list_head *node, *safe;
+    for (node = head->next, safe = node->next; node != head && safe != head;
+         node = node->next,
+        safe = node->next)  // equivalently shift by two node
+    {
+        // using list_for_each_safe() need extra if(second == head) break;
+        node->prev->next = safe;
+        safe->next->prev = node;
+
+        node->next = safe->next;
+        safe->prev = node->prev;
+
+        node->prev = safe;
+        safe->next = node;
+    }
 }
 
 /* Reverse elements in queue */
-void q_reverse(struct list_head *head) {}
+void q_reverse(struct list_head *head)
+{
+    if (!head || list_empty(head))
+        return;
+
+    struct list_head *node, *safe;
+
+    list_for_each_safe (node, safe, head) {
+        list_move(node, head);
+    }
+}
 
 /* Reverse the nodes of the list k at a time */
 void q_reverseK(struct list_head *head, int k)
 {
     // https://leetcode.com/problems/reverse-nodes-in-k-group/
-<<<<<<< HEAD
-=======
 
     // https://hackmd.io/@vax-r/linux2024-homework1#2024q1-Homework1-lab0
     if ((!head || list_empty(head)) || k <= 1)
@@ -121,7 +257,6 @@ void q_reverseK(struct list_head *head, int k)
         list_splice_tail_init(&tmp, &new_head);
     }
     list_splice_init(&new_head, head);
->>>>>>> e417627 (Help commit)
 }
 
 bool cmp(const char *s1, const char *s2)
@@ -239,10 +374,6 @@ int __merge(struct list_head *l1, struct list_head *l2)
 int q_merge(struct list_head *head, bool descend)
 {
     // https://leetcode.com/problems/merge-k-sorted-lists/
-<<<<<<< HEAD
-    return 0;
-}
-=======
     if (!head || list_empty(head))
         return 0;
     else if (list_is_singular(head))
@@ -263,4 +394,3 @@ int q_merge(struct list_head *head, bool descend)
     }
     return queue_size;
 }
->>>>>>> e417627 (Help commit)
